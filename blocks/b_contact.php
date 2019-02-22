144a2548245a294215a81c996dcf286786b5090d
Vladimir Samojlov <v-moi@ya.ru>
2019-02-22 23:55:16 +0200

    edit rsa

--- blocks/b_contact.php
+++ blocks/b_contact.php
@@ -3,7 +3,7 @@
 		<div class="row justify-content-center text-center">
 			<div class="col-md-10">
 				<div class="b_contact__title">
-					Хотите, чтобы мы вам перезвонили?
+					Хотите, чтобы мы вам перезвонили? 
 				</div>
 				<div class="b_contact__form">
 					<?php echo do_shortcode('[contact-form-7 id="5" title="Обратный звонок"]'); ?>

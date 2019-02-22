fd6148db6b176bc8d3be76c070f23e0f30ad071a
Vladimir Samojlov <v-moi@ya.ru>
2019-02-22 23:49:08 +0200

    edit form

--- blocks/b_contact.php
+++ blocks/b_contact.php
@@ -6,7 +6,7 @@
 					Хотите, чтобы мы вам перезвонили?
 				</div>
 				<div class="b_contact__form">
-					<?php echo do_shortcode('[contact-form-7 id="19" title="Контактная форма 1"]'); ?>
+					<?php echo do_shortcode('[contact-form-7 id="5" title="Обратный звонок"]'); ?>
 				</div>
 			</div>
 		</div>

b0e3fb085bfbda2817d6aed3d486b1cd97bfb2b5
Vladimir Samojlov <v-moi@ya.ru>
2019-02-23 12:19:14 +0200

    edit news item

--- blocks/news/news-item.php
+++ blocks/news/news-item.php
@@ -1,20 +1,22 @@
 <div class="news__item">
-	<a href="<?php the_permalink(); ?>">
-	<div class="news__item__photo">
-		<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">	
-	</div>
-	<div class="news__item__content">
-		<div class="news__item__title">
-			<?php the_title(); ?>
+	<div class="news__block">
+		<div class="news__item__photo">
+			<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">	
 		</div>
-		<div class="news__item__text">
-			<?php the_content(); ?>
-		</div>
-		<div class="news__item__more">
-			<div class="my-btn-more">
-				Подробнее <i class="fas fa-chevron-right"></i>	
+		<div class="news__item__content">
+			<div class="news__item__title">
+				<?php the_title(); ?>
+			</div>
+			<div class="news__item__text">
+				<?php the_content(); ?>
+			</div>
+			<div class="news__item__more">
+				<a href="<?php the_permalink(); ?>">
+					<div class="my-btn-more">
+						Подробнее <i class="fas fa-chevron-right"></i>	
+					</div>
+				</a>
 			</div>
 		</div>
 	</div>
-	</a>
 </div>

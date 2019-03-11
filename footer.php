8e24d894c5d8205f697ff690855bb2c17157b8c7
Vladimir Samojlov <v-moi@ya.ru>
2019-03-11 20:09:33 +0200

    remove track24 from footer

--- footer.php
+++ footer.php
@@ -4,19 +4,5 @@
     </footer>
     <?php wp_footer(); ?>
   </div>
-  <script type="text/javascript" language="javascript" src="https://api.track24.ru/track24widget.js"></script>
-<script type="text/javascript">
- var track24Options = {
- 'apiKey' : '7b2796ecd1d91ac8cd351ad83fd09967' /* необходимо указать свой apiKey */,
- 'widgetId' : 'track24widget',
- 'domain' : 'you-domain-name.com',
- 'inputId' : 'track24Input',
- 'buttonId' : 'track24Button',
- 'resultId' : 'track24Result',
- 'useCss' : true,
- 'validate' : true,
- 'codePage': 'UTF-8'  /* windows-1251 or UTF-8 */ 
-  }; 
- </script>
 </body>
 </html>
